1c5c6d54ee6808ae49925e8cfa3c8b10a47c6f25
Michael <hovgaard1990@gmail.com>
2022-04-05 10:19:08 +0200

    clean up

--- JS/script.js
+++ JS/script.js
@@ -3,21 +3,12 @@ var myFullpage = new fullpage('#fullpage', {
 
 
 });
+
 /*
-
-window.addEventListener('load', function page1() {
-document.getElementById('section1').innerHTML = `      <div class = "txt" id = "TxtPage1">
-<p>Tiden gik og pludselig stod hun med en kæmpe rund mave og en eksamen lige om hjørnet. Panikken steg, og hun bad til et mirakel fra de højere magter om at alt ville gå godt. 
-       
- Eksamen hun bestod og babyen kom ud. </p> </div>
-         <img src="img/kalender.svg" alt="kalenderen" class="pictures" id="kalender1">
-         <img src="img/kalender2.svg" alt="kalenderen 2" class="pictures" id="kalender2">
-         <img src="img/kalender3.svg" alt="kalender 3" class="pictures" id="kalender3">
-         <img src="img/kalender4.svg" loading="lazy" alt="kalender 4" class="pictures" id="kalender4">
-
-         <img src="img/Hovedperson-01-Hovedperson-glad-gravid.svg" alt="HP er gravid" class="pictures" id="hPgravid">
-       
+window.addEventListener('load', function() {
+document.getElementById('fullpage').innerHTML =
 `
 
+`      
 
 }) */
